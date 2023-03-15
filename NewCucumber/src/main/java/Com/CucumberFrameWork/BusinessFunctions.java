a0ba7bfa468d25b24e1b78d2777c4e939b947bb7
RAKESH <RAKESH@RAKESH-PC>
2023-03-15 21:35:31 +0530

    Commited The project

---
+++ NewCucumber/src/main/java/Com/CucumberFrameWork/BusinessFunctions.java
@@ -0,0 +1,38 @@
+package Com.CucumberFrameWork;
+
+import static D_utils.Constants.config;
+import Com.CucumberFrameWork.BusinessFunctions;
+import static D_utils.Constants.driver;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+
+import D_utils.Library;
+
+
+public class BusinessFunctions {
+	public static void checkUILoader() {
+		int docount = 0;
+		do {
+			driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+			docount = docount + 1;
+			System.out.println("PageLoadinggCheck - counter: " + docount);
+			try {
+				Thread.sleep(100);
+			}catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			if(docount == 500) {
+				Assert.fail("Application taking longer time than usual to load the data");
+
+				break;
+			}
+		}while(driver.findElements(By.xpath("//div[@class='ui active loader']")).size() != 0);
+
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+
+
+}
