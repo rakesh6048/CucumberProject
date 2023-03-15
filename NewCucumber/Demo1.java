2d9ae5184dcdc1482467aaf9973620dd28b6e295
rakesh6048 <68732430+rakesh6048@users.noreply.github.com>
2023-03-16 01:14:10 +0530

    Create Demo1.java
    Cammit Demo1 java class

---
+++ NewCucumber/Demo1.java
@@ -0,0 +1,13 @@
+public class Addition{
+  
+  public static void main(String arg[]){
+    
+    int a=10, b=20;
+    int add=a+b;
+    
+   System.out.println("Addition of Two Number : "+add);
+    
+  }
+}
+    
+    
